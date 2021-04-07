8b149efe51480909f3348c885a40f56ffc5e657c
Carlos Mertens <mertenscarlos@yahoo.com>
2021-04-08 00:59:20 +0200

    Create variables for the classes

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
 'use strict';
+
+const showModal = document.querySelectorAll('.show-modal');
+const modal = document.querySelector('.modal');
+const hidden = document.querySelector('.hidden');
+const closeMdal = document.querySelector('.close-modal');
+const overlay = document.querySelector('.overlay');
+
+// Loop all 3 modals
+for (let i = 0; i < showModal.length; i++)
+  console.log(showModal[i].textContent);
