a35a9999dd3662dc738a4cb366341ac47f54fa9c
Carlos Mertens <mertenscarlos@yahoo.com>
2021-04-08 16:45:32 +0200

    Create handler to open modal

--- script.js
+++ script.js
@@ -3,9 +3,11 @@
 const showModal = document.querySelectorAll('.show-modal');
 const modal = document.querySelector('.modal');
 const hidden = document.querySelector('.hidden');
-const closeMdal = document.querySelector('.close-modal');
+const closeModal = document.querySelector('.close-modal');
 const overlay = document.querySelector('.overlay');
 
-// Loop all 3 modals
+// Loop all 3 modals- attach event listener- open modal
 for (let i = 0; i < showModal.length; i++)
-  console.log(showModal[i].textContent);
+  showModal[i].addEventListener('click', function () {
+    modal.classList.remove('hidden');
+  });
