2d9de23a578a894b102b7f16ccc0f4810b588e1d
Carlos Mertens <mertenscarlos@yahoo.com>
2021-04-08 16:51:01 +0200

    Create functionality for the overlay

--- script.js
+++ script.js
@@ -2,7 +2,6 @@
 
 const showModal = document.querySelectorAll('.show-modal');
 const modal = document.querySelector('.modal');
-const hidden = document.querySelector('.hidden');
 const closeModal = document.querySelector('.close-modal');
 const overlay = document.querySelector('.overlay');
 
@@ -10,9 +9,11 @@ const overlay = document.querySelector('.overlay');
 for (let i = 0; i < showModal.length; i++)
   showModal[i].addEventListener('click', function () {
     modal.classList.remove('hidden');
+    overlay.classList.remove('hidden');
   });
 
 // Close modal
 closeModal.addEventListener('click', function () {
   modal.classList.add('hidden');
+  overlay.classList.add('hidden');
 });
