2e07d9e9d05243990036d0fd1cbb5226afe2afef
Carlos Mertens <mertenscarlos@yahoo.com>
2021-04-08 17:06:14 +0200

    Refactor: Create funtions to open/close

--- script.js
+++ script.js
@@ -5,15 +5,22 @@ const modal = document.querySelector('.modal');
 const closeModal = document.querySelector('.close-modal');
 const overlay = document.querySelector('.overlay');
 
-// Loop all 3 modals- attach event listener- open modal
-for (let i = 0; i < showModal.length; i++)
-  showModal[i].addEventListener('click', function () {
-    modal.classList.remove('hidden');
-    overlay.classList.remove('hidden');
-  });
+// Function to open model
+const handleOpen = function () {
+  modal.classList.remove('hidden');
+  overlay.classList.remove('hidden');
+};
 
-// Close modal
-closeModal.addEventListener('click', function () {
+// Function to close model
+const handleClose = function () {
   modal.classList.add('hidden');
   overlay.classList.add('hidden');
-});
+};
+
+// Open Model- Loop all modals
+for (let i = 0; i < showModal.length; i++)
+  showModal[i].addEventListener('click', handleOpen);
+
+// Close modal
+closeModal.addEventListener('click', handleClose);
+overlay.addEventListener('click', handleClose);
